77debfb3e376e9b82888e627ebdd8cc23dc24043
enaguero <enaguero@gmail.com>
2019-08-17 18:28:19 +0000

    Basic authentication using flask

--- src/main.py
+++ src/main.py
@@ -2,12 +2,16 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
-from flask import Flask, request, jsonify, url_for
+from flask import Flask, request, jsonify, url_for, g
+
+# Meaning of g object: https://flask.palletsprojects.com/en/1.0.x/api/#flask.g
+
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db, Person
+from flask_httpauth import HTTPBasicAuth
+from models import db, User
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -16,6 +20,20 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
+auth = HTTPBasicAuth()
+
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    # first try to authenticate by token
+    user = User.verify_auth_token(username_or_token)
+    if not user:
+        # try to authenticate with username/password
+        user = User.query.filter_by(username=username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
 
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
@@ -35,6 +53,46 @@ def handle_person():
     }
 
     return jsonify(response_body), 200
+
+
+@app.route('/sign_up', methods=['POST'])
+def new_user():
+    username = request.json.get('username')
+    password = request.json.get('password')
+    if username is None or password is None:
+        abort(400)    # missing arguments
+    if User.query.filter_by(username=username).first() is not None:
+        abort(400)    # existing user
+    user = User(username=username)
+    user.hash_password(password)
+    db.session.add(user)
+    db.session.commit()
+
+    return (jsonify({'username': user.username}),
+            201,
+            {'Location': url_for('get_user', id=user.id, _external=True)})
+
+
+@app.route('/api/users/<int:id>')
+def get_user(id):
+    user = User.query.get(id)
+    if not user:
+        abort(400)
+    return jsonify({'username': user.username})
+
+
+@app.route('/api/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token(20000)
+    return jsonify({'token': token.decode('ascii'), 'duration': 20000})
+
+
+@app.route('/api/resource')
+@auth.login_required
+def get_resource():
+    return jsonify({'data': 'Hello, %s!' % g.user.username})
+
 
 # this only runs if `$ python src/main.py` is exercuted
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -1,5 +1,9 @@
 from flask_sqlalchemy import SQLAlchemy
 
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import (TimedJSONWebSignatureSerializer
+                          as Serializer, BadSignature, SignatureExpired)
+
 db = SQLAlchemy()
 
 # class Person(db.Model):
@@ -15,3 +19,35 @@ db = SQLAlchemy()
 #             "username": self.username,
 #             "email": self.email
 #         }
+
+MY_SECRET_KEY = "I AM SECRET :) :)"
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True)
+    password_hash = db.Column(db.Text)
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=20000):
+        s = Serializer(MY_SECRET_KEY, expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(MY_SECRET_KEY)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return None    # valid token, but expired
+        except BadSignature:
+            return None    # invalid token
+        print("HOLIIIIII")
+        print(data['id'])
+        user = User.query.get(data['id'])
+        return user
